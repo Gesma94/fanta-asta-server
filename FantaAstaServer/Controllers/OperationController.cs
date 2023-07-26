127c30a938eccba7bf6f8b444b775e85bfc3939c
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:18:18 +0200

    F!! Add Operation and User controller

---
+++ FantaAstaServer/Controllers/OperationController.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAstaServer.Interfaces;
+using FantaAstaServer.Models;
+using FantaAstaServer.Models.Domain;
+using FantaAstaServer.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FantaAstaServer.Controllers
+{
+    [Route("api/operation/")]
+    public class OperationController : Controller
+    {
+        private readonly IDbUnitOfWork _dbUnitOfWork;
+
+        public OperationController(IDbUnitOfWork dbUnitOfWork) => _dbUnitOfWork = dbUnitOfWork;
+
+        [HttpPost]
+        [Route("create-auction")]
+        public IActionResult CreateAuction(FormCollection formCollection)
+        {
+            var formFile = formCollection.Files.GetFile("players");
+
+            using var stream = formFile.OpenReadStream();
+            using var streamReader = new StreamReader(stream);
+
+            var fileContent = streamReader.ReadToEnd();
+
+                        var players = JsonSerializer.Deserialize<IEnumerable<FootballerEntity>>(fileContent, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return Ok("Connection OK");
+        }
+
+        [HttpPost]
+        [Route("test-auction")]
+        public IActionResult TestRead()
+        {
+            var auctions = _dbUnitOfWork.Auctions.GetAll();
+            return Ok(auctions);
+        }
+    }
+}
---
+++ FantaAstaServer/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAstaServer.Interfaces;
+using FantaAstaServer.Models.APIs;
+using FantaAstaServer.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace FantaAstaServer.Controllers
+{
+    [Route("v1/user/")]
+    public class UserController : Controller
+    {
+        private readonly IDbUnitOfWork _dbUnitOfWork;
+
+
+        public UserController(IDbUnitOfWork dbUnitOfWork) => _dbUnitOfWork = dbUnitOfWork;
+
+
+        [HttpGet]
+        [Route("create")]
+        public async Task<IActionResult> Create(CreateUserDto createUserDto)
+        {
+            var newUserEntity = new UserEntity()
+            {
+                Username = createUserDto.Username,
+                Email = createUserDto.Email,
+                Password = createUserDto.Password,
+                City = createUserDto.City,
+                FavouriteTeam = createUserDto.FavouriteTeam,
+                DateOfBirth = createUserDto.DateOfBirth,
+                CraetionDate = DateTime.UtcNow
+            };
+
+            await _dbUnitOfWork.Users.Create(newUserEntity);
+            await _dbUnitOfWork.SaveChanges();
+
+            return Ok("Connection OK");
+        }
+    }
+}
