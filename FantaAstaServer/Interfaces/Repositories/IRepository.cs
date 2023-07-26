38aff252dfafa1b04ec959541de9b9f8993c01d8
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:16:35 +0200

    F!! Add repositories interface

---
+++ FantaAstaServer/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using System.Linq;
+using System.Threading.Tasks;
+using FantaAstaServer.Models.Domain;
+
+namespace FantaAstaServer.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        Task<bool> Create(T entity);
+        bool Update(T entity);
+        Task<bool> Delete(int key);
+        Task<T> Get(int key);
+        IQueryable<T> GetAll();
+    }
+}
