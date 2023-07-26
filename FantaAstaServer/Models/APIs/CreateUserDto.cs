d6c0bb53a8c2dad1ab1fcaa0a202874d79b6170d
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:15:43 +0200

    F!! Add CreateUserDto API model

---
+++ FantaAstaServer/Models/APIs/CreateUserDto.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using System;
+
+namespace FantaAstaServer.Models.APIs
+{
+    public record CreateUserDto(string Username, string Password, string Email, DateOnly DateOfBirth, string FavouriteTeam, string City);
+}
