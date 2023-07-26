d8e5009450c2fb3f5bd037b1c1f2058674530607
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:15:20 +0200

    F!! Remove old domain model

--- FantaAstaServer/Models/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) 2023 - Gesma94
-// This code is licensed under MIT license (see LICENSE for details)
-
-using FantaAstaServer.Enums;
-
-namespace FantaAstaServer.Models
-{
-    public record Player(string FirstName, string LastName, string Team, Role Role, int Quotation);
-}
