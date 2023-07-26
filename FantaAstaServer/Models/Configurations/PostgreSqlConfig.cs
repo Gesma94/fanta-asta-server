968fe6430c16bfb2c5408e9786ff5a591858f137
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:16:55 +0200

    F!! Add PostgreSql configuration model setting

---
+++ FantaAstaServer/Models/Configurations/PostgreSqlConfig.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+namespace FantaAstaServer.Models.Settings
+{
+    public class PostgreSqlConfig
+    {
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+    }
+}
