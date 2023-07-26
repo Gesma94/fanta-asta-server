7b38654d73887aa3a2bf3222d04061959563e249
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:17:39 +0200

    R!! Update JsonConsumer

--- FantaAstaServer/Interfaces/IJsonConsumer.cs
+++ FantaAstaServer/Interfaces/IJsonConsumer.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright (c) 2023 - Gesma94
 // This code is licensed under MIT license (see LICENSE for details)
 
-using FantaAstaServer.Models;
 using System.Collections.Generic;
+using FantaAstaServer.Models.Domain;
 
 namespace FantaAstaServer.Interfaces
 {
     public interface IJsonConsumer
     {
-        public IEnumerable<Player> GetPlayerCatalog(string jsonString);
+        public IEnumerable<FootballerEntity> GetPlayerCatalog(string jsonString);
     }
 }
--- FantaAstaServer/Services/JsonConsumer.cs
+++ FantaAstaServer/Services/JsonConsumer.cs
@@ -1,20 +1,21 @@
 ﻿// Copyright (c) 2023 - Gesma94
-// This code is licensed under MIT license (see LICENSE for details)
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
 
 using System.IO;
 using FantaAstaServer.Models;
 using FantaAstaServer.Interfaces;
 using System.Collections.Generic;
+using FantaAstaServer.Models.Domain;
 
 namespace FantaAstaServer.Services
 {
     public class JsonConsumer : IJsonConsumer
     {
-        public IEnumerable<Player> GetPlayerCatalog(string jsonPath)
+        public IEnumerable<FootballerEntity> GetPlayerCatalog(string jsonPath)
         { 
             var jsonFileContent = File.ReadAllText(jsonPath);
 
-            return System.Array.Empty<Player>();
+            return System.Array.Empty<FootballerEntity>();
         }
     }
 }
