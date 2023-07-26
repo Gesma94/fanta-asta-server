c852ad94bb678dd9f1387c2ce686d9dbcf475289
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:19:12 +0200

    F!! Add services in DI container

--- FantaAstaServer/Program.cs
+++ FantaAstaServer/Program.cs
@@ -1,11 +1,22 @@
 // Copyright (c) 2023 - Gesma94
-// This code is licensed under MIT license (see LICENSE for details)
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using FantaAstaServer.Services;
+using Microsoft.EntityFrameworkCore;
+using FantaAstaServer.Services.Repositories;
+using FantaAstaServer.Interfaces.Repositories;
+using FantaAstaServer.Interfaces;
+using Microsoft.Extensions.Configuration;
+using FantaAstaServer.Models.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+var postgreSqlConfig = builder.Configuration.GetSection("postgresql").Get<PostgreSqlConfig>();
+var npgSqlConnectionString = $"User Id={postgreSqlConfig.Id};Password={postgreSqlConfig.Password};Server={postgreSqlConfig.Server};Port={postgreSqlConfig.Port};Database={postgreSqlConfig.Database}";
 
 // Add services to the container.
 
@@ -13,6 +24,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<FantaAstaDbContext>(x => x.UseNpgsql(connectionString: npgSqlConnectionString));
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IAuctionRepository, AuctionRepository>();
+builder.Services.AddTransient<IDbUnitOfWork, DbUnitOfWork>();
 
 var app = builder.Build();
 
