b5423d9473b5a9ea18c21b3a9bfe2c6d0a899785
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:22:05 +0200

    T Update test

--- FantaAstaServer/Controllers/TestController.cs
+++ FantaAstaServer/Controllers/TestController.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2023 - Gesma94
 // This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
 
-using FantaAstaServer.Models.Settings;
+using FantaAstaServer.Models.Configurations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
