95ad499da874a42c7663e23db5c0f691625b6b43
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-26 21:17:52 +0200

    F!! Add unit of work interface

---
+++ FantaAstaServer/Interfaces/IDbUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAstaServer.Interfaces.Repositories;
+using System.Threading.Tasks;
+
+namespace FantaAstaServer.Interfaces
+{
+    public interface IDbUnitOfWork
+    {
+        IAuctionRepository Auctions { get; }
+        IUserRepository Users { get; }
+        Task<int> SaveChanges();
+    }
+}
