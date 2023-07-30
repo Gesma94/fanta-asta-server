6cb5377c54ea555c6ff6ae028de1ec1b39bfb9b0
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-30 23:50:12 +0200

    F!! Update Program.cs

--- FantaAstaServer/Program.cs
+++ FantaAstaServer/Program.cs
@@ -11,11 +11,12 @@ using FantaAstaServer.Interfaces.Repositories;
 using FantaAstaServer.Interfaces;
 using Microsoft.Extensions.Configuration;
 using FantaAstaServer.Models.Configurations;
+using FantaAstaServer.Common.Constants;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-var postgreSqlConfig = builder.Configuration.GetSection("postgresql").Get<PostgreSqlConfig>();
+var postgreSqlConfig = builder.Configuration.GetSection(Constants.PostgresqlConfigKey).Get<PostgreSqlConfig>();
 var npgSqlConnectionString = $"User Id={postgreSqlConfig.Id};Password={postgreSqlConfig.Password};Server={postgreSqlConfig.Server};Port={postgreSqlConfig.Port};Database={postgreSqlConfig.Database}";
 
 // Add services to the container.
@@ -30,6 +31,10 @@ builder.Services.AddDbContext<FantaAstaDbContext>(x => x.UseNpgsql(connectionStr
 
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IAuctionRepository, AuctionRepository>();
+builder.Services.AddTransient<IBatchRepository, BatchRepository>();
+builder.Services.AddTransient<IFootballerRepository, FootballerRepository>();
+builder.Services.AddTransient<IOfferRepository, OfferRepository>();
+builder.Services.AddTransient<IUserActionRepository, UserActionRepository>();
 builder.Services.AddTransient<IDbUnitOfWork, DbUnitOfWork>();
 
 var app = builder.Build();
