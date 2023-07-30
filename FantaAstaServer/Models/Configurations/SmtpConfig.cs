7786b00b7a7f300f375f6b72d1ad8f43330b49e5
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-30 23:48:08 +0200

    F Add SmptConfig model

---
+++ FantaAstaServer/Models/Configurations/SmtpConfig.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+namespace FantaAstaServer.Models.Configurations
+{
+    public class SmtpConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+    }
+}
