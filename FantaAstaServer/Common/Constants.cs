f124c106ecc51a324f53a1c19ed29b9e6a9ef7f4
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-30 23:47:13 +0200

    F Add constants for config keys

---
+++ FantaAstaServer/Common/Constants.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+namespace FantaAstaServer.Common.Constants
+{
+    public class Constants
+    {
+        public const string SmptConfigKey = "smpt";
+        public const string PostgresqlConfigKey = "postgresql";
+
+    }
+}
