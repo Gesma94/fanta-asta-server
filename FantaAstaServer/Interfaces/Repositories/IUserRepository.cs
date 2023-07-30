93f01fded2dfabee25eaf3644e7b443b2fcea67f
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-07-30 23:48:59 +0200

    F!! Add GetByEmail to IUserRepository

--- FantaAstaServer/Interfaces/Repositories/IUserRepository.cs
+++ FantaAstaServer/Interfaces/Repositories/IUserRepository.cs
@@ -2,10 +2,12 @@
 // This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
 
 using FantaAstaServer.Models.Domain;
+using System.Threading.Tasks;
 
 namespace FantaAstaServer.Interfaces.Repositories
 {
     public interface IUserRepository : IRepository<UserEntity>
     {
+        Task<UserEntity> GetByEmail(string email);
     }
 }
