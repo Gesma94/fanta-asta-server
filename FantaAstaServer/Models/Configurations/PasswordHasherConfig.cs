a72a200540cb6e6bc45d045a3c817a3f7bd30007
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-08-02 01:19:35 +0200

    F!! Add PasswordHasherConfig model

---
+++ FantaAstaServer/Models/Configurations/PasswordHasherConfig.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+namespace FantaAstaServer.Models.Configurations
+{
+    public class PasswordHasherConfig
+    {
+        public string Pepper { get; set; }
+        public int Iterations { get; set; }
+    }
+}
