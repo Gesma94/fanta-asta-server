f40124d94b682fff3f7887c95f6e1e7be16f2d53
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-08-27 01:39:03 +0200

    F!! Change CreateUserDto from record to class

--- FantaAstaServer/Models/APIs/CreateUserDto.cs
+++ FantaAstaServer/Models/APIs/CreateUserDto.cs
@@ -1,9 +1,22 @@
 ﻿// Copyright (c) 2023 - Gesma94
 // This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
 
+using FantaAstaServer.JsonConverters;
 using System;
+using System.Text.Json.Serialization;
 
 namespace FantaAstaServer.Models.APIs
 {
-    public record CreateUserDto(string Username, string Password, string Email, DateOnly DateOfBirth, string FavouriteTeam, string City);
+    //public record CreateUserDto(string Username, string Password, string Email, [property:JsonConverter(typeof(DateOnlyJsonConverter))] DateOnly DateOfBirth, string FavouriteTeam, string City);
+    public class CreateUserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        [JsonConverter(typeof(DateOnlyJsonConverter))]
+        public DateOnly DateOfBirth { get; set; }
+        public string FavouriteTeam { get; set; }
+        public string City { get; set; }
+    }
 }
