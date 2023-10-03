8ea3735227da839d6a537a1ca55603ff38754e5e
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-10-03 22:36:00 +0200

    F!! Add AuctionMapper service

---
+++ FantaAstaServer/Interfaces/Services/Mappers/IAuctionMapper.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAstaServer.Models.Domain;
+using FantaAstaServer.Models.DTOs;
+using System.Threading.Tasks;
+
+namespace FantaAstaServer.Interfaces.Services.Mappers
+{
+    public interface IAuctionMapper
+    {
+        Task<UserAuctionDetailsDto> ToUserAuctionDetailsDto(AuctionEntity auctionEntity);
+    }
+}
---
+++ FantaAstaServer/Services/Mappers/AuctionMapper.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAstaServer.Interfaces;
+using FantaAstaServer.Interfaces.Services.Mappers;
+using FantaAstaServer.Models.Domain;
+using FantaAstaServer.Models.DTOs;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FantaAstaServer.Services.Mappers
+{
+    public class AuctionMapper : IAuctionMapper
+    {
+        private readonly IDbUnitOfWork _dbUnitOfWork;
+
+
+        public AuctionMapper(IDbUnitOfWork dbUnitOfWork)
+            => (_dbUnitOfWork) = (dbUnitOfWork);
+
+
+        public async Task<UserAuctionDetailsDto> ToUserAuctionDetailsDto(AuctionEntity auctionEntity)
+        {
+            var auctionUsers = await _dbUnitOfWork.UserAuctions.GetByAuctionId(auctionEntity.Id);
+
+            return new UserAuctionDetailsDto() 
+            {
+                Name = auctionEntity.Name,
+                Status = auctionEntity.Status,
+                UserAmount = auctionEntity.UserAmount,
+                CurrentUserAmount = auctionUsers.Count(),
+            };
+
+        }
+    }
+}
