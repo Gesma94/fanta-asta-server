2e34a2ff269a5e5cd5a6fe6bb89ac93d3fab8c5c
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-10-12 00:56:57 +0200

    F!! Add MediatR

---
+++ FantaAsta.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FantaAsta.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+        return services;
+    }
+}
