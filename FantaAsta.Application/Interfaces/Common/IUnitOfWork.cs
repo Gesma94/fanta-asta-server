85b726acf03e5121d59ad784f110f4abf2fc627d
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-10-12 00:51:48 +0200

    F!! Add IUnitOfWork

---
+++ FantaAsta.Application/Interfaces/Common/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAsta.Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace FantaAsta.Application.Interfaces.Common;
+
+public interface IUnitOfWork
+{
+    IAuctionRepository Auctions { get; }
+    IBatchRepository Batches { get; }
+    IFootballerRepository Footballers { get; }
+    IFootballerUserRepository FootballerUsers { get; }
+    IOfferRepository Offers { get; }
+    IUserAuctionRepository UserAuctions { get; }
+    IUserRecoveryGuidRepository UserRecoveryGuids { get; }
+    IUserRepository Users { get; }
+    Task<IDbContextTransaction> BeginTransactionAsync();
+    Task RollbackTransactionAsync();
+    Task<int> SaveAsync();
+}
