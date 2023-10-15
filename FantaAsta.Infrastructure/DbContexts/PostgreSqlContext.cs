4190c3196d11cb89bed6e0969d6a2908bda58c6b
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-10-15 20:45:10 +0200

    R Format PostgreSqlContext.cs

--- FantaAsta.Infrastructure/DbContexts/PostgreSqlContext.cs
+++ FantaAsta.Infrastructure/DbContexts/PostgreSqlContext.cs
@@ -12,51 +12,49 @@ namespace FantaAsta.Infrastructure.DbContexts;
 public class PostgreSqlContext : DbContext
 {
     private readonly PostgreSqlOptions _postgreSqlOptions;
+    
+    public PostgreSqlContext(IOptions<PostgreSqlOptions> postgreSqlConfig, DbContextOptions<PostgreSqlContext> dbContextOptions)
+        : base(dbContextOptions)
+    { 
+         _postgreSqlOptions = postgreSqlConfig?.Value ?? throw new ArgumentNullException(nameof(postgreSqlConfig));
+    }
 
-        public PostgreSqlContext(IOptions<PostgreSqlOptions> postgreSqlConfig, DbContextOptions<PostgreSqlContext> dbContextOptions)
-            : base(dbContextOptions)
-        { 
-             _postgreSqlOptions = postgreSqlConfig?.Value ?? throw new ArgumentNullException(nameof(postgreSqlConfig));
-        }
-
-        
-        public DbSet<UserEntity> Users { get; set; }
-        public DbSet<OfferEntity> Offers { get; set; }
-        public DbSet<BatchEntity> Batches { get; set; }
-        public DbSet<AuctionEntity> Auctions { get; set; }
-        public DbSet<FootballerEntity> Footballers { get; set; }
-        public DbSet<UserAuctionEntity> UserAuctions { get; set; }
-        public DbSet<FootballerUserEntity> FootballerUsers { get; set; }
-        public DbSet<UserRecoveryGuidEntity> UserRecoveryGuids { get; set; }
-
-
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<OfferEntity> Offers { get; set; }
+    public DbSet<BatchEntity> Batches { get; set; }
+    public DbSet<AuctionEntity> Auctions { get; set; }
+    public DbSet<FootballerEntity> Footballers { get; set; }
+    public DbSet<UserAuctionEntity> UserAuctions { get; set; }
+    public DbSet<FootballerUserEntity> FootballerUsers { get; set; }
+    public DbSet<UserRecoveryGuidEntity> UserRecoveryGuids { get; set; }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
         {
-            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            switch (entry.State)
             {
-                switch (entry.State)
-                {
-                    case EntityState.Added:
-                        entry.Entity.CreatedTime = DateTimeOffset.UtcNow;
-                        entry.Entity.LastModifiedTime = DateTimeOffset.UtcNow;
-                        break;
-                    case EntityState.Modified:
-                        entry.Entity.LastModifiedTime = DateTimeOffset.UtcNow;
-                        break;
-                }
+                case EntityState.Added:
+                    entry.Entity.CreatedTime = DateTimeOffset.UtcNow;
+                    entry.Entity.LastModifiedTime = DateTimeOffset.UtcNow;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedTime = DateTimeOffset.UtcNow;
+                    break;
             }
-
-            return base.SaveChangesAsync(cancellationToken);
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(_postgreSqlOptions.GetConnectionString());
-            base.OnConfiguring(optionsBuilder);
-        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostgreSqlContext).Assembly);
-        }    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_postgreSqlOptions.GetConnectionString());
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostgreSqlContext).Assembly);
+    }    
 }
