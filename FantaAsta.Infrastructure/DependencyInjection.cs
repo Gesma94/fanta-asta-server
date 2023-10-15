23961c533b44591315ca3cdbf5d90c7f9f9c8954
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-10-15 23:24:17 +0200

    F!! Add DependencyInjection in Infrastructure layer

---
+++ FantaAsta.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under CC BY-NC-ND 4.0 license (see LICENSE for details)
+
+using FantaAsta.Application.Interfaces.Repositories;
+using FantaAsta.Infrastructure.DbContexts;
+using FantaAsta.Infrastructure.Options;
+using FantaAsta.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FantaAsta.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddOptions<PostgreSqlOptions>();
+        services.AddDbContext<PostgreSqlContext>();
+
+        services.AddTransient<IAuctionRepository, AuctionRepository>();
+        services.AddTransient<IBatchRepository, BatchRepository>();
+        services.AddTransient<IFootballerRepository, FootballerRepository>();
+        services.AddTransient<IFootballerUserRepository, FootballerUserRepository>();
+        services.AddTransient<IOfferRepository, OfferRepository>();
+        
+        return services;
+    }
+}
